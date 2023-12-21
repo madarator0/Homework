d1074b265b367af77a4ddee3c76b1f297feebaa5
madarator0 <124123253+madarator0@users.noreply.github.com>
2023-12-21 21:46:25 +0600

    Update Dz_Omer6.cpp

--- Dz_Omer6.cpp
+++ Dz_Omer6.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
-    vector<int> res1;
     int a, res = 0;
     cin >> a;
     while (a != 0) {
-        res1.push_back(a % 10);
+        res += a % 10;
         a /= 10;
-    }
-    for (int i = 0; i < res1.size(); i++) {
-        res += res1[i];
     }
     cout << res;
     return 0;
